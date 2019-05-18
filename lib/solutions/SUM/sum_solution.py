7fb667282c414c5b811197f4f28489c62783be67
sbx_user1051 <sbx_user1051@169.254.230.69>
2019-05-18 11:11:11 +0000

    Sat May 18 11:11:11 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    return x + y
+
+
+assert compute(0, 0) == 0
+assert compute(20, 0) == 20
+assert compute(0, 20) == 20
+assert compute(90, 20) == 110
+
