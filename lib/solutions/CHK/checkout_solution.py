3e2872ae467f47852204d5fecc3a5633651a86d7
sbx_user1051 <sbx_user1051@169.254.16.29>
2019-05-19 13:07:41 +0000

    Sun May 19 13:07:41 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,10 +10,36 @@ price_data = {
     'D': 15,
     'E': 40,
     'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
 }
+
 special_price = {
     'A': [(5, 200), (3, 130)],
     'B': [(2, 45)],
+    'H': [(10, 80), (5, 45)],
+    'K': [(2, 150)],
+    'P': [(5, 200)],
+    'Q': [(3, 80)],
+    'V': [(3, 130), (2, 90)],
 }
 
 
@@ -27,6 +53,28 @@ def trim_and_count(skus):
     return counter
 
 
+def apply_special_discounts(counter):
+    #  2E get one B free
+    # strip out the Bs if we have sufficient Es exist here:
+    number_of_es = counter.get('E', 0)
+    counter['B'] = max(0, counter['B'] - (number_of_es//2))
+
+    # strip out the Fs if we have sufficient Fs exist here:
+    # 2F get one F free
+    counter['F'] = max(0, int(math.ceil(counter['F']*2.0/3.0)))
+
+    #  3N get one M free
+    number_of_ns = counter.get('N', 0)
+    counter['M'] = max(0, counter['M'] - (number_of_ns//3))
+
+    #  3R get one Q free
+    number_of_rs = counter.get('R', 0)
+    counter['Q'] = max(0, counter['Q'] - (number_of_rs//3))
+
+    # 3U get one U free
+    counter['U'] = max(0, int(math.ceil(counter['U']*2.0/3.0)))
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -35,16 +83,7 @@ def checkout(skus):
     except Exception:
         return -1
 
-    #  2E get one B free
-    # strip out the Bs if we have sufficient Es exist here:
-    number_of_es = counter.get('E', 0)
-    counter['B'] = max(0, counter['B'] - (number_of_es//2))
-
-    # strip out the Fs if we have sufficient Fs exist here:
-    # 2F get one F free
-    number_of_fs = counter.get('F', 0)
-    counter['F'] = max(0, int(math.ceil(counter['F']*2.0/3.0)))
-
+    apply_special_discounts(counter)
 
     total = 0
 
@@ -71,7 +110,7 @@ assert checkout('FFEFFFF') == 40+40
 assert checkout('BEEBE') == 80+40+30
 assert checkout("A") == 50
 assert checkout("AB") == 50+30
-assert checkout("ABZ") == -1
+assert checkout("AB0") == -1
 assert checkout("£$%") == -1
 assert checkout("AAA") == 130
 assert checkout("AAAA") == 130+50
@@ -88,6 +127,11 @@ assert checkout('EEB') == 80
 assert checkout('EEBEEEE') == 80*3
 assert checkout('BEEBEE') == 80*2
 assert checkout('BEEBE') == 80+40+30
+assert checkout('UUVV') == 40*2 + 90
+assert checkout('UUVVUU') == 40*3 + 90
+assert checkout('P') == 50
+assert checkout('PPPPPP') == 50+200
+
 
 
 
